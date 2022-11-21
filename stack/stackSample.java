1dc151271afa4c1031c5ace5a7f495888e4acc0f
Jahn Crystan Abella <2224514@slu.edu.ph>
2022-11-21 12:07:10 +0800

    updated classes

--- stack/stackSample.java
+++ stack/stackSample.java
@@ -1,4 +1,34 @@
 package stack;
 
+import java.util.Stack;
+
 public class stackSample {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> s = new Stack<>();
+
+        s.push(2);
+        s.push(83);
+        s.push(22);
+        s.push(30);
+        s.push(1);
+        s.push(98);
+
+        // Last/top element will always print first!
+
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+
+
+
+
+
+
+
+    }
 }
