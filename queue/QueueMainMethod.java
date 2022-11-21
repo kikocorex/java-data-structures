341c178ac246a713a8b2dd2bfd9779bf2d51d7fe
Jahn Crystan Abella <2224514@slu.edu.ph>
2022-11-21 17:01:04 +0800

    Fixed QueueMainMethod.java

--- queue/QueueMainMethod.java
+++ queue/QueueMainMethod.java
@@ -1,7 +1,23 @@
 package queue;
 
 public class QueueMainMethod {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        CircularQueueImplementation q = new CircularQueueImplementation();
+
+        q.insert(2);
+        q.insert(3);
+        q.insert(100);
+        q.insert(92);
+
+        q.display();
+
+        System.out.println(q.remove());
+        q.insert(133);
+        q.display();
+
+        System.out.println(q.remove());
+        q.insert(99);
+        q.display();
     }
 }
