42caa8cf0fc800b694c1289aa84af640f74501f1
kiko <2224514@slu.edu.ph>
2023-01-24 23:07:39 +0800

    OOP

---
+++ OOP/Human.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public interface Human {
+
+    void setName(String name);
+    void setAge(int age);
+    void setGender(String gen);
+    void setBirthday(String bday);
+    void setWeight(double weight);
+}
